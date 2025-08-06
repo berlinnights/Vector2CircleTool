987721d80105a0d3e9818a86a137a0073f1708e0
Moe <65114599+berlinnights@users.noreply.github.com>
2025-08-06 19:51:12 +0200

    Update vector2plugin.gd

--- vector2plugin.gd
+++ vector2plugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 
-@onready var inspector_plugin: Vector2InspectorPlugin #= load("res://addons/vector2_circle_inspector_tool/vector2_circle_tool.gd")
+@onready var inspector_plugin: Vector2InspectorPlugin
 
 func _enter_tree():
 	inspector_plugin = Vector2InspectorPlugin.new()
